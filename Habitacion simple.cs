d703ae6bb9018481401b06e04bab8fb39e266d4e
Vitia Martinez <Vitia Martinez@DESKTOP-UL0N2S6>
2024-08-07 09:58:22 -0600

    Agregar archivos de proyecto.

---
+++ Habitacion simple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    internal class Habitacion_simple : Habitacion
+    {
+        public Habitacion_simple(int numeroHabitacion, double precioNoche, bool disponibilidad, string cliente, int numeroCama)
+        {
+            Numero = numeroHabitacion;
+            PrecioPorNoche = precioNoche;
+            Disponible = disponibilidad;
+        }
+
+        public int NumeroCamas { get; set; }
+
+    }
+}
---
+++ Habitacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    public class Habitacion 
+    {
+        public int Numero { get; set; }
+        public double PrecioPorNoche { get; set; }
+        public bool Disponible { get; set; }
+        public string ClienteAsignado { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("");
+        }
+
+
+
+    }
+}
